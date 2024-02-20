61c9c14a8c98de72a690d1237bdccc21007bad84
Vexumi <glebas.35211@gmail.com>
2024-02-20 20:27:45 +0700

    added: signin view, viewmodel

---
+++ Emporium/ViewModels/SignInViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Emporium.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+
+namespace Emporium.ViewModels
+{
+    public class SignInViewModel : BaseViewModel
+    {
+        public ICommand SignInButtonCommand { get; set; }
+
+        private string _email;
+        private string _password;
+
+        public string Email
+        {
+            get { return _email; }
+            set
+            {
+                _email = value;
+                OnPropertyChanged("Email");
+            }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set
+            {
+                _password = value;
+                OnPropertyChanged("Password");
+            }
+        }
+
+        public SignInViewModel()
+        {
+            SignInButtonCommand = new RelayCommand(o => SignIn());
+        }
+
+        public void SignIn()
+        {
+            throw new DivideByZeroException("SignInMethod");
+        }
+    }
+}
---
+++ Emporium/Views/SignInWindow.xaml.cs
@@ -0,0 +1,19 @@
+﻿using Emporium.ViewModels;
+
+using System.Windows;
+
+namespace Emporium.Views
+{
+    /// <summary>
+    /// Логика взаимодействия для LoginWindow.xaml
+    /// </summary>
+    public partial class SignInWindow : Window
+    {
+        public SignInWindow()
+        {
+            InitializeComponent();
+
+            DataContext = new SignInViewModel();
+        }
+    }
+}
