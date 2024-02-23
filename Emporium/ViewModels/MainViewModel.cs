6ac9a620daa9131a9cfda87b2e2433db41feff74
Vexumi <glebas.35211@gmail.com>
2024-02-23 12:28:40 +0700

    added: swapping windows

--- Emporium/ViewModels/MainViewModel.cs
+++ Emporium/ViewModels/MainViewModel.cs
@@ -1,16 +1,21 @@
-﻿using Emporium.Models;
-using Emporium.Views;
+﻿using Emporium.Infrastructure;
+using Emporium.Infrastructure.Enums;
+using Emporium.Models;
 using Emporium.Views.UserControls;
+using System;
+using System.Collections.Generic;
 using System.Windows;
 using System.Windows.Controls;
-using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
-
+using System.Windows.Input;
+using static Emporium.Infrastructure.WindowExtensions;
 namespace Emporium.ViewModels
 {
-    public class MainViewModel: BaseViewModel
+    public class MainViewModel : BaseViewModel
     {
         private User _user;
         private Window _view;
+
+        public ICommand OpenWindowCommand { get; set; }
 
         public User CurrentUser
         {
@@ -26,6 +31,41 @@ namespace Emporium.ViewModels
         {
             this._view = view;
             this.CurrentUser = user;
+
+            this.OpenWindowCommand = new RelayCommand(o => OpenWindow(Enum.Parse<WindowType>(o.ToString())));
+        }
+
+        private void OpenWindow(WindowType windowType)
+        {
+            switch (windowType)
+            {
+                case WindowType.Products:
+                    {
+                        foreach (DockPanel tb in FindVisualChildren<DockPanel>(this._view))
+                        {
+                            ClearPanel(tb);
+                            var uc = new ProductsControl();
+                            tb.Children.Add(uc);
+                            return;
+                        }
+                        break;
+                    }
+                default: break;
+            }
+        }
+
+        private void ClearPanel(Panel panel)
+        {
+            List<UIElement> elementsToRemove = new();
+            foreach (UIElement el in panel.Children)
+            {
+                elementsToRemove.Add(el);
+            }
+
+            foreach (var item in elementsToRemove)
+            {
+                panel.Children.Remove(item);
+            }
         }
     }
 }
